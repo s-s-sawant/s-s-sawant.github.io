a6a299a6e86e260a0d8eb6c5acfa97cd0171882a
Sagar-math <sagarssawant1995@gmail.com>
2024-10-01 14:50:28 +0530

    talk generator using tsv

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -5,6 +5,11 @@ var addressPoints = [
     76.9628425
   ],
   [
+    "Chennai, India",
+    13.0836939,
+    80.270186
+  ],
+  [
     "Nainital, India.",
     29.29499495,
     79.41625112686134
@@ -15,13 +20,38 @@ var addressPoints = [
     8.752653
   ],
   [
+    "Adyar, India",
+    13.00645,
+    80.2577791
+  ],
+  [
+    "Champs-Sur-Marne, France",
+    48.8526892,
+    2.6027219
+  ],
+  [
     "Auroville, India.",
     12.0073927,
     79.8099471
   ],
   [
+    "Matunga, India",
+    19.0274356,
+    72.8501467
+  ],
+  [
+    "Paris, France",
+    48.8534951,
+    2.3483915
+  ],
+  [
     "Kalavakkam, Tamilnadu, India.",
     12.7453188,
     80.1950454
+  ],
+  [
+    "Mumbai, India",
+    19.0785451,
+    72.878176
   ]
 ];
