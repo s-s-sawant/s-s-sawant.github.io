18d406f0679aa6b0434ae491c5fa9e4cda9b35dd
Sagar <sagarssawant1995@gmail.com>
2025-01-15 11:22:02 +0530

    commit.sh change

--- commit.sh
+++ commit.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# Define a timestamp function
-timestamp() {
-  date +"%T" # current time
-}
+read -p "Enter commit msg" cmt
 
 cd
 ssh-add think_pad
 cd git/s-s-sawant.github.io
 
 git add --all
-git commit -m "Updated: `date +'%Y-%m-%d %H:%M:%S'`" # Updated: 2019-08-28 10:22:06
+git commit -m "$cmt" 
 git push origin master
