cdfdcdd4a69be63646560d8600adb0d6be442de2
Sagar <sagarssawant1995@gmail.com>
2025-01-15 10:11:22 +0530

    Updated: 2025-01-15 10:11:22

--- commit.sh
+++ commit.sh
@@ -6,8 +6,8 @@ timestamp() {
 }
 
 cd
-ssh-add priv
-cd my_git/webpage/s-s-sawant.github.io
+ssh-add thinkpad
+cd git/s-s-sawant.github.io
 
 git add --all
 git commit -m "Updated: `date +'%Y-%m-%d %H:%M:%S'`" # Updated: 2019-08-28 10:22:06
